5776396a89d81d618d30cabdef0925d3f102403b
Alina Gasperino <alina.gasperino@gmail.com>
2022-06-08 18:35:48 -0400

    Server setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
+
+const apiRouter = require('./routes/api');
+
+const PORT = 3000;
+
+// handle parsing of the request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
+
 // serve index.html on the route '/'
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+// define route handlers
+app.use('/api', apiRouter);
 
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+/**
+ * express global error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+ app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
