7c65cce2e0a40dfee87831982e03a82db712006f
Alina Gasperino <alina.gasperino@gmail.com>
2022-06-08 18:39:52 -0400

    Adding react router routes

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello Alina!!</h1>
-    )
-  }
-}
+import CreateJournalEntry from './CreateJournalEntry';
+import Journal from './Journal';
+
+const App = props => {
+  return (
+    <div className="router">
+      <main>
+        {/*
+            NOTE: The syntax below is for React-Router library
+            https://reacttraining.com/react-router/web/guides/quick-start
+        */}
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={Journal}
+          />
+          <Route
+            exact
+            path="/entry"
+            component={CreateJournalEntry}
+          />
+        </Switch>
+      </main>
+    </div>
+  );
+};
 export default App;
