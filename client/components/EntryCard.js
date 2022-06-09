7490877daa6a688e2aa0358298827838e06b70a6
Alina Gasperino <alina.gasperino@gmail.com>
2022-06-09 19:02:32 -0400

    Attempting to create a new entry

--- client/components/EntryCard.js
+++ client/components/EntryCard.js
@@ -1,25 +1,42 @@
 import React from 'react';
 
-const EntryCard = ({
-  info
-}) => {
-  const { title, body } = info;
+const EntryCard = (props) => {
+  const { title, body } = props;
   
   return (
-    <article className="card entryCard">
-      <div className="entryHeadContainer">
-        <h3 className="entryName">Title: {title}</h3>
+    <div className="card" >
+    {
+      title &&  
+      <div className="card-header">{title}</div>
+    }
+    {
+      desc &&
+      <div className="card-body">
+        <p className="card-text">{body}</p>
       </div>
-      <div className="cardBody">
-        <ul className="entryDetailsList">
-          <li className="entryDetail">Body: {body}</li>
-        </ul>
-        </div>
-      <div className="entryCardFooter">
-        <p>Date</p>
-      </div>
-    </article>
+    }
+  </div>
   );
 }
 
 export default EntryCard;
+
+// <article className="card entryCard">
+// {
+//   title &&
+//   <div className="entryHeadContainer">
+//     <h3 className="entryName">Title: {title}</h3>
+//   </div>
+// }
+// {
+//   body &&
+// <div className="cardBody">
+//   <ul className="entryDetailsList">
+//     <li className="entryDetail">Body: {body}</li>
+//   </ul>
+//   </div>
+// }
+// <div className="entryCardFooter">
+//   <p>Date</p>
+// </div>
+// </article>
--- client/components/Journal.js
+++ client/components/Journal.js
@@ -9,56 +9,96 @@ class Journal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fetchedEntries: false,
-      entries: []
+      // fetchedEntries: false,
+      entries: [
+        {
+          title: "Title 0",
+          body: "Body 0"
+        }
+      ]
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000/api')
-      .then(res => res.json())
-      .then((entries) => {
-        if (!Array.isArray(entries)) entries = [];
-        return this.setState({
-          entries,
-          fetchedEntries: true
-        });
-      })
-      .catch(err => console.log('Journal.componentDidMount: get entries: ERROR ', err));
+  addCard = () => {
+    const entries = this.state.entries
+    entries.push({
+      title: "Title 10",
+      body: "Body 10"
+    })
+    this.setState({ entries });
   }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api')
+  //     .then(res => res.json())
+  //     .then((entries) => {
+  //       if (!Array.isArray(entries)) entries = [];
+  //       return this.setState({
+  //         entries,
+  //         fetchedEntries: true
+  //       });
+  //     })
+  //     .catch(err => console.log('Journal.componentDidMount: get entries: ERROR ', err));
+  // }
   
   render() {
-    if (!this.state.fetchedEntries) return (
-      <><p>Loading journal entries, please wait...</p></>
-    );
+    // if (!this.state.fetchedEntries) return (
+    //   <><p>Loading journal entries, please wait...</p></>
+    // );
 
     const { entries } = this.state;
 
-    if (!entries.length) return (
-      <><p>Sorry, no journal entries found.</p></>
-    );
+    // if (!entries.length) return (
+    //   <><p>Sorry, no journal entries found.</p></>
+    // );
 
-    const entryEl = entries.map((entry, i) => {
-      return (
-        <EntryCard
-          key={i}
-          info={entry}     
-        />
-      );
-    });
+    // const entryEl = entries.map((entry, i) => {
+    //   return (
+    //     <EntryCard
+    //       key={i}
+    //       info={entry}     
+    //     />
+    //   );
+    // });
 
     return (
       <section className="mainSection">
-      <div className="entryContainer">
-      <article className="addCard card entryCard">
-        <svg viewBox="0 0 24 24" className="plus">
-    <path d="M 12 2 L 12 22 M 2 12 L 22 12" stroke="gray" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" />
-  </svg>
-      </article>
-        {entryEl}
-      </div>
-    </section>
-    );
+        <div className="entryContainer">
+        <article className="addCard card entryCard" onClick={this.addCard}>
+          <svg viewBox="0 0 24 24" className="plus">
+            <path d="M 12 2 L 12 22 M 2 12 L 22 12" stroke="gray" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" />
+          </svg>
+        </article>
+          {entries.map((entry, i) => {
+            return (
+              <EntryCard
+                key={i}
+                title={entry.title}
+                body={entry.body}    
+              />
+              )
+            })
+          }
+        </div>
+      </section> 
+    )
   }
 }
  export default Journal;
+
+ 
+
+{/* 
+return (
+<section className="mainSection">
+<div className="entryContainer">
+<article className="addCard card entryCard" onClick={this.addCard}>
+  <svg viewBox="0 0 24 24" className="plus">
+    <path d="M 12 2 L 12 22 M 2 12 L 22 12" stroke="gray" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" />
+  </svg>
+</article>
+  {entryEl}
+  </div>
+</section> 
+);
+*/}
